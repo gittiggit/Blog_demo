ceabeeb453b302e5c79e2f061cc0b9134c326abb
gittiggit <mailtommrr@gmail.com>
2016-01-09 17:47:28 +0600

    Something

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  #before_save :load_article
+  before_action :set_article, only: [:edit, :update, :destroy, :show]
   
   def index
     @articles = Article.all
@@ -20,15 +20,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find params[:id]
   end
 
   def edit
-    @article = Article.find params[:id]
   end
 
   def update
-    @article = Article.find params[:id]
     if @article.update article_params
       flash[:success] = "Updated"
       redirect_to article_path @article
@@ -38,7 +35,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find params[:id]
     @article.destroy
     flash[:notice] = "Article deleted"
     redirect_to articles_path
@@ -50,7 +46,7 @@ class ArticlesController < ApplicationController
     params.require(:article).permit :title, :description
   end
 
-  # def load_article
-  #   @article = Article.find params[:id]
-  # end
+  def set_article
+    @article = Article.find params[:id]
+  end
 end
