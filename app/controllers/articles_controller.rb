4023547c0b9efc446d4b607196e261d651bea5ff
gittiggit <mailtommrr@gmail.com>
2016-01-09 17:35:11 +0600

    Something

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  #before_save :load_article
+  
+  def index
+    @articles = Article.all
+  end
   def new
     @article = Article.new
   end
@@ -7,29 +12,45 @@ class ArticlesController < ApplicationController
     #render plain: params[:article].inspect
     @article = Article.new article_params
     if @article.save 
-      redirect_to articles_path
+      flash[:success] = "Added successfully"
+      redirect_to article_path(@article)
     else
       render :new
     end  
   end
 
-  def index
-  end
-
   def show
+    @article = Article.find params[:id]
   end
 
   def edit
+    @article = Article.find params[:id]
   end
 
   def update
+    @article = Article.find params[:id]
+    if @article.update article_params
+      flash[:success] = "Updated"
+      redirect_to article_path @article
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @article = Article.find params[:id]
+    @article.destroy
+    flash[:notice] = "Article deleted"
+    redirect_to articles_path
+
   end
 
   private
   def article_params
     params.require(:article).permit :title, :description
   end
+
+  # def load_article
+  #   @article = Article.find params[:id]
+  # end
 end
