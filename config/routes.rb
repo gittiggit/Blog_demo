af599c0ce530548225a8b4642da7e49b23954b00
gittiggit <mailtommrr@gmail.com>
2016-01-11 21:13:21 +0600

    Commited after creatin and destroying session through sessions controller for the login/logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   root "welcomes#home"
   resources :articles
 	resources :users, except:[:new] # because there is a named route for the user/new
-	get "signup", to: "users#new"
 
+	get "login", to: "sessions#new"
+	post "login", to: "sessions#create"
+	delete "signout", to: "sessions#destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
