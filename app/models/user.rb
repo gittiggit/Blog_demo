9b7ddec759eb4b61ecd12e2cac6df23b4f3389f9
gittiggit <mailtommrr@gmail.com>
2016-01-11 10:42:11 +0600

    Commited after adding validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+	validates :username, presence: true, uniqueness: {case_sensetive: false},
+						length: {minimum: 3, maximum: 25}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 25},
+		        uniqueness: {case_sensitive: false},
+		        format: {with: VALID_EMAIL_REGEX}
 end
