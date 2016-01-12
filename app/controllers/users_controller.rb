ee2caec202859a5f341b410d46b5c0cdc5fbf127
gittiggit <contactwithmahmud@gmail.com>
2016-01-12 12:33:00 +0600

    Commited after taking a signed up user as a logged_in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params
-    if @user.save
-      flash[:success] = "User created successfully"
-      redirect_to articles_path
-    else
-      render :new
-    end
+      if @user.save
+        session[:user_id] = @user.id
+        flash[:success] = "User created successfully"
+        redirect_to @user
+      else
+        render :new
+      end
   end
 
   def edit
