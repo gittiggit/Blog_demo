4a80235925257dee9335eddb156e6301dd23687c
gittiggit <contactwithmahmud@gmail.com>
2016-01-12 14:31:11 +0600

    After giving admin to access all resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111074813) do
+ActiveRecord::Schema.define(version: 20160112070234) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -32,9 +32,10 @@ ActiveRecord::Schema.define(version: 20160111074813) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
