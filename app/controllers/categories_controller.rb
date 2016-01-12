2551fb5a47164063afb7693e15393664105d6dc8
gittiggit <contactwithmahmud@gmail.com>
2016-01-12 18:20:15 +0600

    Commited after adding categories, index, new, create, admin authorization to it

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,52 @@
+class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
+
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = "Successfully added"
+      redirect_to categories_path
+    else
+      flash[:notice] = "Category not added"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+  def category_params
+    params.require(:category).permit :name
+  end
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:danger] = "Only admin can perform this action"
+      redirect_to categories_path
+    end
+  end
+end
+
+# def require_admin
+#   if !logged_in? || (logged_in? and !current_user.admin?)
+#     flash[:danger] = "Only admin can perform this action"
+#     redirect_to categories_path
+#   end
+# end
+# def require_admin
+#   if !logged_in? and !current_user.admin?
+#     flash[:danger] = "Only admin can perform this action"
+#     redirect_to categories_path
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'welcomes/about'
 
   root "welcomes#home"
+  resources :categories, except: [:destroy]
   resources :articles
 	resources :users, except:[:new] # because there is a named route for the user/new
   get "signup", to: "users#new"
