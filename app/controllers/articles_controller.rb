47fac144302006e6a22e570c2a6d0a079e0c8752
gittiggit <contactwithmahmud@gmail.com>
2016-01-12 12:19:16 +0600

    Commited after adding authorization on the resources(users and articles)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :destroy, :show]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
     @articles = Article.paginate(page: params[:page], per_page: 2) #.where(created_at: :desc)
@@ -11,7 +13,7 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new article_params
-    @article.user = User.first
+    @article.user = current_user
     if @article.save 
       flash[:success] = "Added successfully"
       redirect_to article_path(@article)
@@ -49,5 +51,12 @@ class ArticlesController < ApplicationController
 
   def set_article
     @article = Article.find params[:id]
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:notice] = "You can only edit/delete your own article"
+      redirect_to root_path
+    end
   end
 end
